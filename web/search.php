827b54d5b80bca4e3e5d77e222a883bdad87be91
Enrico Ros <enrico.ros@gmail.com>
2016-07-22 10:52:45 +0000

    Improve the looks of the search page.

--- web/search.php
+++ web/search.php
@@ -22,7 +22,6 @@ require_once __DIR__ . '/../indexer/jobs_interface.php';
 define('METHOD_SEARCH_QUERY', 'q');
 
 $query = isset($_GET[METHOD_SEARCH_QUERY]) ? $_GET[METHOD_SEARCH_QUERY] : '';
-
 ?>
 
 <!doctype html>
@@ -37,92 +36,62 @@ $query = isset($_GET[METHOD_SEARCH_QUERY]) ? $_GET[METHOD_SEARCH_QUERY] : '';
 </head>
 <body>
 <div class="header">
-    <h3 class="row">Crawling + Indexing Control</h3>
+    <h3 class="row">Search jam::ccv3</h3>
 </div>
 
 <div class="row" style="margin-top: 1em;">
-    <!-- systems status panel -->
+
+    <!-- Main Search Box -->
+    <div class="row">
+        <div class="medium-6 columns">
+            <input id="query-input" type="text" placeholder="Search for the jam ..." value="<?= $query ?>">
+        </div>
+        <div class="medium-6 columns">
+            <div id="index-button" class="button tiny round warning" onclick="performSearch();">Search</div>
+            <span class="button tiny round" onclick="location.reload(); return false;">refresh</span>
+        </div>
+    </div>
+
+    <!-- unneeded 'what we need to add' section -->
+    <div class="row">
+        <div class="medium-12 columns">
+            <h5>Search for:</h5>
+            <ul>
+                <li>
+                    <i class="set-to-query">I love you back</i>, <i class="set-to-query">knock knock knock</i>,
+                    <i class="set-to-query">570 wifi base stations</i>, <i class="set-to-query">rapists</i>,
+                    <i class="set-to-query">you are going to fail</i>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- search outputpanel -->
     <div class="panel radius">
-        <div>Crawlers and Indexers are
-            <pre>
+        <div>
             <?php
             if ($query != '') {
                 require_once __DIR__ . '/../indexer/IndexMachine_ElasticSearch.php';
                 $indexMachine = new \IndexMachine_ElasticSearch(['173.230.144.120:9199']);
 
-                $search = $indexMachine->search($query, false, 200, 4);
+                $search = $indexMachine->search($query, true, 200, 4);
+                $strictSnipperCount = $search['stats']['snippetsCount'];
+                if ($strictSnipperCount < 10) {
+                    echo 'Repeating the search but with more relax, since we only got ' . $strictSnipperCount . ' ..';
+                    $search = $indexMachine->search($query, false, 200, 4);
+                }
+
                 $jString = json_encode($search, JSON_PRETTY_PRINT);
-                echo $jString;
+                echo '<pre>' . $jString . '</pre>';
+            } else {
+                echo "Search output";
             }
+            //            for ($i = 0; $i < $maxWorkers; $i++) {
+            //                echo "<div class='w-block " . ($i < $activeWorkers ? "w-active" : "") . "'>" . ($i + 1) . "</div>&nbsp;";
+            //            }
             ?>
-            </pre>
-
-            <?php
-//            for ($i = 0; $i < $maxWorkers; $i++) {
-//                echo "<div class='w-block " . ($i < $activeWorkers ? "w-active" : "") . "'>" . ($i + 1) . "</div>&nbsp;";
-//            }
-            ?>
-            <span class="refresh-button" onclick="location.reload(); return false;">refresh</span>
         </div>
     </div>
-    <br>
-
-    <!-- Main Search Box -->
-    <div class="row">
-        <div class="medium-6 columns">
-            <input id="query-input" type="text" placeholder="Closed captions Query ..." value="<?=$query?>">
-        </div>
-        <div class="medium-6 columns">
-            <div id="index-button" class="button tiny round warning" onclick="addToIndex();">+Add to Crawling Queue
-            </div>
-            &nbsp;
-            <div class="button tiny round" onclick="tryYoutube();">Try on Youtube</div>
-        </div>
-        <div class="medium-12 columns">
-            Hint: to search within the close captions and index those videos use
-            <i class="set-to-query">"text to search...",cc</i>.
-        </div>
-    </div>
-    <br>
-
-    <!-- unneeded 'what we need to add' section -->
-    <div class="row">
-        <div class="medium-6 columns">
-            <h5>What we need to add:</h5>
-            <ul>
-                <li>
-                    <i class="set-to-query">cult movies</i>, <i class="set-to-query">tv shows</i>,
-                    <i class="set-to-query">youtube channels</i>, <i class="set-to-query">cartoons</i>,
-                    <i class="set-to-query">memes</i>
-                </li>
-                <li>
-                    <i class="set-to-query">science</i>, <i class="set-to-query">technology</i>,
-                    <i class="set-to-query">entertainment</i>, <i class="set-to-query">funny</i>,
-                    <i class="set-to-query">futuristic</i>
-                </li>
-                <li>
-                    <i class="set-to-query">Steve Jobs</i>, <i class="set-to-query">Obama</i>,
-                    <i class="set-to-query">Elon Musk</i>, <i class="set-to-query">Donald Trump</i>,
-                    <i class="set-to-query">PewDiePie</i>
-                </li>
-            </ul>
-        </div>
-        <div class="medium-6 columns">
-            <h5>To remember:</h5>
-            <ul>
-                <li>570 wifi base stations</li>
-                <li>you are going to fail</li>
-            </ul>
-        </div>
-    </div>
-
-    <!-- footer -->
-    <div style="color: #888">
-        Each query is searched 4 times by { relevance, viewCount, rating, and date }.<br>
-        Search is restricted to videos that are: { close captioned, HD, embeddable, in english }.<br>
-        Safe-search is off.
-    </div>
-    <br>
 </div>
 
 <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
@@ -130,7 +99,10 @@ $query = isset($_GET[METHOD_SEARCH_QUERY]) ? $_GET[METHOD_SEARCH_QUERY] : '';
     var $queryInput = $('#query-input');
     var $indexButton = $('#index-button');
 
-    function addToIndex() {
+    function performSearch() {
+        var query = $queryInput.val();
+        if (query.length > 1)
+            window.location.search = '<?=METHOD_SEARCH_QUERY?>=' + encodeURI(query);
     }
 
     // enter on query to add to index
@@ -145,19 +117,9 @@ $query = isset($_GET[METHOD_SEARCH_QUERY]) ? $_GET[METHOD_SEARCH_QUERY] : '';
     });
 
     // try to focus the search box asap
-//    setTimeout(function() {
-//        $queryInput.focus();
-//    }, 100);
-
-    function tryYoutube() {
-        var query = $queryInput.val();
-        if (query.length > 1) {
-            // open the new window
-            var url = 'https://www.youtube.com/results?search_query=' + encodeURI(query);
-            var win = window.open(url, '_blank');
-            win.focus();
-        }
-    }
+    setTimeout(function () {
+        $queryInput.focus();
+    }, 100);
 </script>
 
 </body>
